c2bda8b9f331b6d8c4e86eb86002a793170856b5
あずき⪥™ <44765629+hijiki02@users.noreply.github.com>
2022-01-20 21:58:47 +0900

    fix: 正常にマイグレーションが実行できない不具合を修正

--- src/cli/migrations.ts
+++ src/cli/migrations.ts
@@ -3,11 +3,19 @@ import { Migrations } from "conf/dist/source/types"
 const migrations: Migrations<Record<string, unknown>> = {
 	"1.1.0": store =>
 	{
-		store.set("webSocketUrl", store.get("instance"))
-		store.delete("instance")
+		// instance -> webSocketUrl
+		if (store.has("instance"))
+		{
+			store.set("webSocketUrl", store.get("instance"))
+			store.delete("instance")
+		}
 
-		store.set("notifier", store.get("method"))
-		store.delete("method")
+		// method -> notifier
+		if (store.has("method"))
+		{
+			store.set("notifier", store.get("method"))
+			store.delete("method")
+		}
 	},
 }
 
